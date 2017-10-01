9b40ef051d698423bf7c56c034325c9b9145eacd
ShyamBantupalli <ShyamBantupalli@DESKTOP-75SJTS8>
2017-10-01 21:59:52 +0530

    1st Commit

--- Location/Controllers/HomeController.cs
+++ Location/Controllers/HomeController.cs
@@ -21,30 +21,15 @@ namespace Location.Controllers
            // var url = "https://timezoneapi.io/api/ip/?ip=5.9.31.87";
 
             using (var client = new System.Net.WebClient())
-
-
-
-
             {
-              //  var resp = client.DownloadData(url);
-              //  byte[] by = resp;
-              //  string str = System.Text.Encoding.ASCII.GetString(resp);
-
+             
                 var response = client.DownloadData(url);
-                var b = 0;
-                var abc = 1234;
-                var b = 0;
-
-
-
                 var response1 = Encoding.Default.GetString(response);
                 JavaScriptSerializer oJS = new JavaScriptSerializer();
-                //RootObject oRootObject = new RootObject();
                 var oRootObject = oJS.Deserialize<RootObject>(response1);
                 var _country = oRootObject.data.country;
                 var _currency = oRootObject.data.timezone.currency_alpha_code;
-                var ip = oRootObject.data.ip;
-            
+                var ip = oRootObject.data.ip;       
                 return View(oRootObject);
             }
         }
