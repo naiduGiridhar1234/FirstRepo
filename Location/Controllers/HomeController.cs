9bc63410822064166baf2a564e7aad90d234defb
ShyamBantupalli <ShyamBantupalli@DESKTOP-75SJTS8>
2017-10-01 13:55:43 +0530

    3rdChanges

--- Location/Controllers/HomeController.cs
+++ Location/Controllers/HomeController.cs
@@ -32,6 +32,7 @@ namespace Location.Controllers
 
                 var response = client.DownloadData(url);
                 var b = 0;
+                var abc = 1234;
                 var response1 = Encoding.Default.GetString(response);
                 JavaScriptSerializer oJS = new JavaScriptSerializer();
                 //RootObject oRootObject = new RootObject();
